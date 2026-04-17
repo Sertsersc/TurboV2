56848fd5874b0d64c681dcee5ce465f27150a2f3
Sertsersc <musab@example.com>
2026-04-17 21:52:09 +0300

    Turbo v2.0 - GitHub Actions Derleme Sistemi

---
+++ app/build/generated/source/buildConfig/debug/com/bariscraft/turbo/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bariscraft.turbo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bariscraft.turbo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2.0";
+  // Field from default config.
+  public static final String GROQ_API_KEY = "KEY";
+}
